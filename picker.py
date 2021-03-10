78daf64ba84f8cba409b8d71b059c9ac20b67c0e
AngrierEngineer <vawkes@gmail.com>
2021-03-10 12:13:09 -0500

    Updating parseing

---
+++ picker.py
@@ -0,0 +1,11 @@
+import sys
+import argparse
+
+vermsg = "Picker - Version 0.1"
+ 
+# Initialize parser
+parser = argparse.ArgumentParser(description = vermsg)
+# Adding optional argument
+parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
+# Read arguments from command line
+args = parser.parse_args()
