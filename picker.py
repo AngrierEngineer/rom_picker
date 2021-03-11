4093e360694c200523a6b470e4fa72d2ee2cd692
AngrierEngineer <vawkes@gmail.com>
2021-03-10 19:50:22 -0500

    arg work

--- picker.py
+++ picker.py
@@ -7,12 +7,10 @@ vermsg = "Picker - Version 0.1"
 parser = argparse.ArgumentParser(description = vermsg)
 # Adding optional argument
 parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
+parser.add_argument('-in', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
+parser.add_argument('-of', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
+parser.add_argument('-it', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
+
 
 # Read arguments from command line
 args = parser.parse_args()
