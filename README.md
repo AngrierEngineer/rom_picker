cf90b1bfc9f8d42b3aeeb742fcdba5533643b523
AngrierEngineer <AngrierEngineer@github.com>
2021-03-18 20:06:28 -0400

    main

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Rom Picker
-This is part of a Python projects that aims to handle the gap between archive grade ROM dumps and roms that are playable by current generation emulations. 
+This is part of a Python projects that aims to handle the gap between archive grade ROM dumps and roms that are playable by current generation emulations. The goal is to be able to take a archived image and convert it to a playable image per request. This relies hevealy on MAME's CHDMAN tool to convert the image into a playable format, so the emulator must be able to support the CHD format. This tool alsoaims to change one compression to another so that older collections can be converted to new compressions meathods as they become avalible. 
+
+Planned features:
+- 7z/zip/rar convertion to chd for image based ROMs
+- 7z/zip/rar to 7z/zip/rar convertion
+- Maintaining a cache of converted images so that the coversion doesn't have to be done every time
+- "Cleaning up" old images when space quota is hit.
+- Intergration into Retro Pi nd other SBC projects.
+- Batch conversion of files to avoid long wait times.
+
+Limitations:
+- To my knowledge there isn't any python based libraries that allow for direct CHD conversion so this program will have to be a wrapper program until such a thing exsist. If anyone has a good way to use the C++ code in python, please let me know.
+- Since this tool uses CHDMAN, all the limitations CHD has, this tool will also have.
+- Due to the nature of compression and limited resources of SBC, conversion will always be time consuming. 
+- Due to limited memory, sbc will most likely not be able to use memory to temerary files.
+- Network communications will depend on samba or nfs mounted shares.
+
+
--- picker.py
+++ picker.py
@@ -1,16 +1,50 @@
 import sys
+import os
+import time
+import subprocess
 import argparse
+import fnmatch
+import tempfile
+import configparser
 
-vermsg = "Picker - Version 0.1"
+vermsg = '''Picker - Version 0.1
+
+Conversion tool for image based ROMs.
+
+By Matthew Greenlaw (AngrierEngineer)'''
  
 # Initialize parser
 parser = argparse.ArgumentParser(description = vermsg)
 # Adding optional argument
-parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-in', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-of', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-parser.add_argument('-it', default="CHD", type=str, metavar='EXT', help = "Output file type for conversion")
-
+parser.add_argument('-ot', default="CHD", type=str, metavar='EXT', help = "The type of file you're out putting too. (Some files may not be supported based on selected system)")
+parser.add_argument('-op', default="CHD", type=str, metavar='PATH', help = "The path for the output file. Default: [Currect Directory]")
+parser.add_argument('-it', default="CHD", type=str, metavar='EXT', help = "Type of file you wish to convert. Default: [Auto]")
+parser.add_argument('-ip', default="CHD", type=str, metavar='PATH', help = "The path for the iutput file. Default: [Currect Directory]")
+parser.add_argument("-v", action="store_true", help="Enables verbose mode.")
 
 # Read arguments from command line
 args = parser.parse_args()
+
+#Setup verbose mode
+verboseprint = print if args.v else lambda *a, **k: None
+
+#Setup of config.ini
+config = configparser.ConfigParser()
+if os.path.isfile('config.ini'):
+     verboseprint("Config File exist... Skipping")        
+else:
+    verboseprint("Config File missing... Creating")
+    config['DEFAULT'] = {'GDIDir': os.getcwd() + "./",
+                         'CHDDir': os.getcwd() + "./",
+                         'ROMDir': os.getcwd() + "./",
+                         'TMPDir': "/TMP/"}
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+config.read('config.ini')
+GDIDir = config['DEFAULT']['GDIDir']
+CHDDir = config['DEFAULT']['CHDDir']
+ROMDir = config['DEFAULT']['ROMDir']
+TMPDir = config['DEFAULT']['TMPDir']
+
+def get_roms(path, ext)
+    
